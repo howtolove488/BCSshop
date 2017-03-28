1262420f9bd1a32000f3f20ad5778700725a3a0b
Coffee Code <viethoang.nghiem@gmail.com>
2017-03-28 09:57:36 +0700

    Task List Angular app with a secure serverless REST API.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AuthService } from './auth.service';
+import { MaterialModule } from '@angular/material';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-list/task-form/task-form.component';
+
+import { Http, RequestOptions } from '@angular/http';
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
+import { TaskListService } from './task-list/task-list.service';
+
+// creates a factory to AuthHttp
+export function authHttpFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp(new AuthConfig(), http, options);
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    TaskListComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    SlimLoadingBarModule.forRoot(),
+    MaterialModule.forRoot(),
     HttpModule
   ],
-  providers: [],
+  providers: [
+      AuthService,
+      TaskListService, // adds new service
+      {
+        provide: AuthHttp,
+        useFactory: authHttpFactory, // defines how to provide AuthHttp
+        deps: [ Http, RequestOptions ]
+      }
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
